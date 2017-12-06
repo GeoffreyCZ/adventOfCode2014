ffb0195843c6a6f7e5f34a5ffe41affcc9721238
Michal Kroupa <awatar.trellac@gmail.com>
2017-12-06 14:55:53 +0100

    part two resolved

--- src/Tasks/DayFour.php
+++ src/Tasks/DayFour.php
@@ -35,7 +35,40 @@ class DayFour implements DayI
 
     public function solvePartTwo()
     {
-        // TODO: Implement solvePartTwo() method.
+        $numberOfValid = count($this->input);
+        foreach ($this->input as $line) {
+            $length = count($line);
+            foreach ($line as $key => $value) {
+                for ($i = $key + 1; $i < $length; $i++) {
+                    $currentLineValue = $line[$i];
+                    if (strlen($value) === strlen($currentLineValue)) {
+                        $firstArrayOfChars = str_split($value, 1);
+                        $secondArrayOfChars = str_split($currentLineValue, 1);
+                        for ($j = 0; $j < count($firstArrayOfChars); $j++) {
+                            for ($k = 0; $k < count($secondArrayOfChars); $k++) {
+                                if ($firstArrayOfChars[$j] === $secondArrayOfChars[$k]) {
+                                    unset($firstArrayOfChars[$j]);
+                                    $firstArrayOfChars = array_values($firstArrayOfChars);
+                                    unset($secondArrayOfChars[$k]);
+                                    $secondArrayOfChars = array_values($secondArrayOfChars);
+                                    $j = -1;
+                                    $k = -1;
+                                    break;
+                                }
+                            }
+                        }
+                        if ((count($firstArrayOfChars) === 0) && (count($secondArrayOfChars) === 0)) {
+                            $numberOfValid--;
+                            break 2;
+//                        } else {
+//                            echo $value . ' a ' . $currentLineValue;
+//                            echo '<br>';
+                        }
+                    }
+                }
+            }
+        }
+        return $numberOfValid;
     }
 
     public function resolve()
