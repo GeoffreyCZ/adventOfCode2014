18c54f8d6fc004dee4a0d920a0804d88dae6f4c2
Michal Kroupa <awatar.trellac@gmail.com>
2017-12-19 21:31:15 +0100

    part one resolved

--- index.php
+++ index.php
@@ -13,32 +13,38 @@ use AdventOfCode\Tasks\DayTwo;
 use AdventOfCode\Tasks\DayFour;
 use AdventOfCode\Tasks\DayFive;
 use AdventOfCode\Tasks\DaySix;
+use AdventOfCode\Tasks\DaySeven;
 
+
+$daySeven = new DaySeven();
+echo '<h3>==========</h3>';
+echo '<h3>Day 7</h3>';
+$daySeven->resolve();
 
 $daySix = new DaySix();
 echo '<h3>==========</h3>';
-echo '<h3>Day six</h3>';
+echo '<h3>Day 6</h3>';
 $daySix->resolve();
 
 
 //$dayFive = new DayFive();
 //echo '<h3>==========</h3>';
-//echo '<h3>Day five</h3>';
+//echo '<h3>Day 5</h3>';
 //$dayFive->resolve();
 
 
 $dayFour = new DayFour();
 echo '<h3>==========</h3>';
-echo '<h3>Day four</h3>';
+echo '<h3>Day 4</h3>';
 $dayFour->resolve();
 
 $dayTwo = new DayTwo();
 echo '<h3>==========</h3>';
-echo '<h3>Day two</h3>';
+echo '<h3>Day 2</h3>';
 $dayTwo->resolve();
 
 $dayOne = new DayOne();
 echo '<h3>==========</h3>';
-echo '<h3>Day one</h3>';
+echo '<h3>Day 1</h3>';
 $dayOne->resolve();
 
---
+++ src/Tasks/DaySeven.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Michal Kroupa
+ * Date: 07.12.2017
+ * Time: 13:15
+ */
+
+namespace AdventOfCode\Tasks;
+
+use AdventOfCode\Helpers\Parser;
+
+class DaySeven implements DayI
+{
+
+    private $input;
+
+    public function solvePartOne()
+    {
+        $array = $this->input;
+        $filtered = [];
+        foreach ($array as $key => $line) {
+            if (count($line) >= 3) {
+                $filtered[] = str_replace(",", "", $line);
+            }
+        }
+
+        $this->input = $filtered;
+
+        $roots = [];
+        foreach ($filtered as $subArray) {
+            $roots[] = $subArray[0];
+        }
+        foreach ($roots as $key => $root) {
+            foreach ($filtered as $key2 => $subArray) {
+                for ($i = 3; $i < count($subArray); $i++) {
+                    if ($root == $subArray[$i]) {
+                        unset($roots[$key]);
+                    }
+                }
+            }
+        }
+        $roots = array_values($roots);
+        return $roots[0];
+    }
+
+    public function solvePartTwo()
+    {
+        $lines = explode(PHP_EOL, $input);
+        $arr = [];
+        foreach ($lines as $line) {
+            if (preg_match('/^(.*?) \((\d+)\)$/', $line, $matches)) {
+                list($_, $name, $weight) = $matches;
+                $arr[$name] = [
+                    'name' => $name,
+                    'weight' => (int)$weight,
+                    'children' => [],
+                ];
+            }
+            elseif (preg_match('/^(.*?) \((\d+)\) -> (.*)$/', $line, $matches)) {
+                list($_, $name, $weight, $children) = $matches;
+                $arr[$name] = [
+                    'name' => $name,
+                    'weight' => (int)$weight,
+                    'children' => array_map('trim', explode(',', $children)),
+                ];
+            }
+        }
+
+        $root = '';
+        foreach ($arr as $name => $el) {
+            $chance = true;
+            foreach ($arr as $el2) {
+                if (in_array($name, $el2['children'])) {
+                    $chance = false;
+                }
+            }
+            if ($chance) {
+                $root = $name;
+            }
+        }
+
+        // calc total weight everywhere
+        $calcWeight = function($el) use (&$calcWeight, &$arr) {
+            $el['total'] = $el['weight'];
+            foreach ($el['children'] as $child) {
+                $calcWeight($arr[$child]);
+                $el['total'] += $arr[$child]['total'];
+            }
+            $arr[$el['name']] = $el;
+        };
+        $calcWeight($arr[$root]);
+
+        $consensus = 0;
+        while (true) {
+            $el = $arr[$root];
+            if (!count($el['children'])) {
+                // el has to change its weight to consensus
+                return $el['weight']  + ($consensus - $el['total']);
+            }
+
+            $weights = array_map(function($e) use ($arr) { return $arr[$e]['total']; }, $el['children']);
+            $min = min($weights);
+            $max = max($weights);
+            if ($min == $max) {
+                // el has to change its weight
+                return $el['weight']  + ($consensus - $el['total']);
+            }
+            $mins = array_filter($el['children'], function ($e) use ($min, $arr) { return $arr[$e]['total'] == $min; });
+            $maxs = array_filter($el['children'], function ($e) use ($max, $arr) { return $arr[$e]['total'] == $max; });
+            if (count($mins) == 1) {
+                // this is the rogue child
+                $consensus = $max;
+                $root = end($mins);
+            }
+            else {
+                // the max one is the rogue child
+                $consensus = $min;
+                $root = end($maxs);
+            }
+        }
+
+        return -1;
+    }
+
+    public function resolve()
+    {
+        $parser = new Parser();
+        $this->input = $parser->parseStringsByNewlineAndSpacesIntoArray('./Input/DaySeven');
+        echo('Part one: ' . $this->solvePartOne());
+        echo '<br>';
+        $this->input2 = $parser->load('./Input/DaySeven');
+        echo('Part two: ' . $this->solvePartTwo());
+    }
+
+
+}
